336f8b7d6071787a6e89311f3c50086c633e5a01
fara0000 <fi@livereach.com>
2022-04-11 16:30:04 +0300

    added shift2dGrid matrix task but haven't solved it yet

---
+++ js/Shift2dGrid(матрица)/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number[][]}
+ */
+var shiftGrid = function(grid, k) {
+
+};
+
+console.log(shiftGrid([[1,2,3],[4,5,6],[7,8,9]], 1));
+console.log(shiftGrid([[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]], 4));
+console.log(shiftGrid([[1,2,3],[4,5,6],[7,8,9]], 9));
